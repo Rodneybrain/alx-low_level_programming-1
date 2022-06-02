56aef4644ec86bcbba21ab7d86e4e0e7212aa9ae
modyy-14 <dankimme16@gmail.com>
2022-06-02 13:26:18 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-/**                                                                                                                              
- * main - Entry point                                                                                                            
- *                                                                                                                               
- * Return: Always 0 (Success)                                                                                                    
- */                                                                                                                              
-int main(void) 
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
