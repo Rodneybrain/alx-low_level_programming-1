940c271a7fcfa0f596d6da2121b4eba88446de65
modyy-14 <58995900+modyy-14@users.noreply.github.com>
2022-06-09 23:46:30 +0300

    every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,28 @@
- 
+#include "main.h"
+
+/**
+ * print_square - a function that prints a square, followed by a new line
+ * 
+ * Return: a square made of '#'
+ */
+void print_square(int size)
+{
+	int er, qt;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (er = 1; er <= size; er++)
+		{
+			_putchar('#');
+			for (qt = 2; qt <= size; qt++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+} 
