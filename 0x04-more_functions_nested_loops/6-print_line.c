d51b024ecd9fd2b0316c376df299680113e916dc
modyy-14 <58995900+modyy-14@users.noreply.github.com>
2022-06-09 23:36:35 +0300

    shortest distance between two points

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,25 @@
- 
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - a function that draws a straight line in the terminal
+ * 
+ * Return: a straight line
+ */
+void print_line(int n)
+{
+	int zf;
+
+	if (w <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (zf = 1; zf <= w; zf++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+} 
