7c6437d58b3f418123ef68c9e69fff569e26e0e7
modyy-14 <58995900+modyy-14@users.noreply.github.com>
2022-06-09 23:39:56 +0300

    parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,28 @@
- 
+#include "holberton.h"
+
+/**
+ * print_diagonal -  a function that draws a diagonal line on the terminal
+ * 
+ * Return: a diagonal
+ */
+void print_diagonal(int n)
+{
+	int zf, q;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (zf = 1; zf <= n; zf++)
+		{
+			for (q = 1; q < zf; q++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+} 
