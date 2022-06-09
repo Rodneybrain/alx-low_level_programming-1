39594fd83c81c38bc9a2e21fbcbabb5efcdf0022
modyy-14 <58995900+modyy-14@users.noreply.github.com>
2022-06-09 23:04:44 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,8 +9,22 @@
 
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-		return (1);
-	else
-		return (0);
+	char ch; 
+    printf("Enter the alphabet:");
+ 
+    scanf("%c",&ch);
+ 
+   if(ch>=65 && ch<=90)
+ 
+      printf("upper case");
+ 
+    else if(ch>=97 && ch<=122)
+ 
+ 
+    printf("lower case");
+ 
+     
+    else
+     printf("Invalid input");
+return 0;
 }
