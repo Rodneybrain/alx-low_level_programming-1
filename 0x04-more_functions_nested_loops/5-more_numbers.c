d30903ebac9871adf5ab83fd5a164d5cd2aa2a01
modyy-14 <58995900+modyy-14@users.noreply.github.com>
2022-06-09 23:31:30 +0300

    universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,24 @@
- 
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14
+ *
+ * Return: 0-14 x10 followed by new line
+ */
+void more_numbers(void)
+{
+	int j, e;
+
+	for (e = 0; e < 10; e++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+} 
