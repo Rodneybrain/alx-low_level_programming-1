8d67b2f07fa3b48dc30652ff75cc6fc710120890
modyy-14 <58995900+modyy-14@users.noreply.github.com>
2022-06-09 23:08:55 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,30 +1,16 @@
 #include <stdio.h>                                                                                                               
-#include "main.h"
-
+#include "main.h"                                                                                                                
+                                                                                                                                 
 /**                                                                                                                              
  * _isupper - a function that checks for uppercase character                                                                     
- *
+ *                                                                                                                               
  * Return: 1 if c is uppercase, 0 otherwise                                                                                      
- */ 
-
-int _isupper(int c)
-{
-	char ch; 
-    printf("Enter the alphabet:");
- 
-    scanf("%c",&ch);
- 
-   if(ch>=65 && ch<=90)
- 
-      printf("upper case");
- 
-    else if(ch>=97 && ch<=122)
- 
- 
-    printf("lower case");
- 
-     
-    else
-     printf("Invalid input");
-return 0;
-}
+ */                                                                                                                              
+                                                                                                                                 
+int _isupper(int c)                                                                                                              
+{                                                                                                                                
+        if ((c >= 'A') && (c <= 'Z'))                                                                                            
+                return (1);                                                                                                      
+        else                                                                                                                     
+                return (0);                                                                                                      
+}    
