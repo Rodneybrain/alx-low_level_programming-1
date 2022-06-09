80fc604c80addc76f8042e24318ed68063605074
modyy-14 <58995900+modyy-14@users.noreply.github.com>
2022-06-09 23:57:09 +0300

    function that prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,31 @@
- 
+#include "main.h"
+
+/**
+ * print_triangle - a function that prints a triangle
+ *
+ * Return: triangle of '#'s
+ */
+void print_triangle(int size)
+{
+	int qr, at, ew;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (at = 0; at <= (size - 1); at++)
+		{
+			for (qr = 0; qr < (size - 1) - at; qr++)
+			{
+				_putchar(' ');
+			}
+			for (ew = 0; ew <= at; ew++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+} 
