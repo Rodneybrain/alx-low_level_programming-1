3264496abad40e3550622fc33f553a3ef40073f2
modyy-14 <58995900+modyy-14@users.noreply.github.com>
2022-06-16 14:23:05 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,19 @@
- 
+ #include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - a function that changes all lower of a string to upper
+ * @n: input string
+ * Return: capitialized string
+ */
+char *string_toupper(char *)
+{
+	int y;
+
+	for (y = 0; n[y] != '\0'; y++)
+	{
+		if (n[y] >= 'a' && n[y] <= 'z')
+			n[y] = n[y] - 32;
+	}
+	return (n);
+}
