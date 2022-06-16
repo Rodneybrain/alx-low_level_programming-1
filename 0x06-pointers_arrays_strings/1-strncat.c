b52f4ac13e44841898944bb98c5793088289f15f
modyy-14 <58995900+modyy-14@users.noreply.github.com>
2022-06-16 14:01:58 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,28 @@
- 
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - a function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * @n: input of max bytes to be used
+ * Return: Always 0 (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, wp;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+	}
+
+	for (wp = 0; wp < n; wp++)
+	{
+		dest[a + wp] = src[wp];
+		if (src[wp] == '\0')
+			wp = n;
+	}
+
+	return (dest);
+}
+
